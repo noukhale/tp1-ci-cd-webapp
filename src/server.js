fb1e04a2ff4d96c93afd3c4a128764f4d4308337
noukhale <abdel.noukhale@gmail.com>
2025-09-20 13:46:10 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -3,18 +3,3 @@ const server = http.createServer((req,res)=>{
 res.end('Hello from CI/CD');
 });
 server.listen(3000);
-Créez un package.json :
-{
-"name": "tp1-ci-cd",
-"version": "1.0.0",
-"description": "Demo CI/CD Azure App Service with Jest",
-"main": "src/server.js",
-"scripts": {
-"start": "node src/server.js",
-"test": "jest --ci",
-"build": "mkdir -p dist && cp -r src/* dist/"
-},
-"devDependencies": {
-"jest": "^29.7.0"
-}
-}
