08723156c58cbbb20c0f260365ada639e442773e
noukhale <abdel.noukhale@gmail.com>
2025-09-20 15:31:53 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,5 +1,9 @@
 const http = require('http');
-const server = http.createServer((req,res)=>{
-res.end('Hello from CI/CD 3');
+const server = http.createServer((req, res) => {
+res.end('Hello from CI/CD');
 });
-server.listen(3000);
+const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
+server.listen(port, host, () => {
+console.log(`Server listening on http://${host}:${port}`);
+});
